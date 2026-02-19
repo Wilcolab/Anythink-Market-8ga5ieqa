56823fa7613c023e7696baafc2233eaead61e386
Hamsaveni P <hamsapurushothama45@gmail.com>
2026-02-19 09:23:24 +0530

    Add function to convert string to camel case

---
+++ basic_prompt.js
@@ -0,0 +1,9 @@
+function toCamelCase(str) {
+  return str
+    .toLowerCase()
+    .split(/[\s_\-]+/)
+    .map((word, index) =>
+      index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
+    )
+    .join("");
+}
